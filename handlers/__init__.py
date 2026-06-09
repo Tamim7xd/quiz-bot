bb3d8774f4d8611b14dde789d006a98ac0a2c33b
Tamim7xd <152809345+Tamim7xd@users.noreply.github.com>
2026-06-09 09:22:03 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,6 @@
-__init__.py
+# ملف تهيئة معالجات الأوامر
+from .user_commands import UserCommands
+from .admin_commands import AdminCommands
+from .owner_panel import OwnerPanel
+
+__all__ = ['UserCommands', 'AdminCommands', 'OwnerPanel']
